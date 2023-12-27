63d0aa8ef139c203fac8c866b8c7d907e7b8b4e4
n9507246 <n9507246@gmail.com>
2023-12-27 18:07:22 +0300

    созданы маршруты и контроллер для пользователей

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-// const {User} = require('./models')
-// app.get('/api/user', async (req,res) => {
-//     res.send(await User.findAll())
-// })
 const routes = require('./router')
 app.use('/api', routes)
 
